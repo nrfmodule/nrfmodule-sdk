4f4cc72655bf4470e9d034f3bd25b11e45fe65b3
V1ncent77 <vincent.cergolj@gmail.com>
2026-08-17 16:05:37 +0200

    port(ncs340): board.cmake runners keyed on SoC Kconfig, correct nrf91 gate symbol
    BOARD_QUALIFIERS string matching stopped registering runners on Zephyr 4.4
    (no runners.yaml, Partition Manager post-build then fails). Condition on
    CONFIG_SOC_* like the in-tree boards. The nrf91 opt-out symbol is
    SOC_SERIES_NRF91 on Zephyr 4.4, not NRF91X.

--- boards/arm/pigeontracker/board.cmake
+++ boards/arm/pigeontracker/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if("${BOARD_QUALIFIERS}" STREQUAL "/nrf52840")
+if(CONFIG_SOC_NRF52840)
   board_runner_args(nrfjprog "--nrf-family=NRF52")
   board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
   board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
@@ -8,11 +8,10 @@ if("${BOARD_QUALIFIERS}" STREQUAL "/nrf52840")
   include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
   include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
   include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-endif()
-
-if("${BOARD_QUALIFIERS}" STREQUAL "/nrf9151" OR "${BOARD_QUALIFIERS}" STREQUAL "/nrf9151/ns")
+else()
+  # nRF9151 targets (secure and ns)
   board_runner_args(nrfjprog "--nrf-family=NRF91")
-  board_runner_args(jlink "--device=nRF9160_xxAA" "--speed=4000")
+  board_runner_args(jlink "--device=nRF9151_xxCA" "--speed=4000")
 
   include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
   include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
